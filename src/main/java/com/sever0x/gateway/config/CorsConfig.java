c25b19aeedda0c9a504afa627397a92c2d35cab0
shdwraze <shdwrazedev@gmail.com>
2024-06-29 21:21:10 +0300

    minor changes

--- src/main/java/com/sever0x/gateway/config/CorsConfig.java
+++ src/main/java/com/sever0x/gateway/config/CorsConfig.java
@@ -1,32 +1,32 @@
-package com.sever0x.gateway.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-@Configuration
-public class CorsConfig {
-
-    @Value("${frontend.url}")
-    private String frontendOriginUrl;
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowCredentials(true);
-        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        cors.setAllowedOrigins(Collections.singletonList(frontendOriginUrl));
-        cors.setAllowedHeaders(Collections.singletonList("*"));
-        cors.setExposedHeaders(Collections.singletonList("Location"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", cors);
-        return new CorsWebFilter(source);
-    }
-}
+//package com.sever0x.gateway.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Value("${frontend.url}")
+//    private String frontendOriginUrl;
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration cors = new CorsConfiguration();
+//        cors.setAllowCredentials(true);
+//        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        cors.setAllowedOrigins(Collections.singletonList(frontendOriginUrl));
+//        cors.setAllowedHeaders(Collections.singletonList("*"));
+//        cors.setExposedHeaders(Collections.singletonList("Location"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", cors);
+//        return new CorsWebFilter(source);
+//    }
+//}
